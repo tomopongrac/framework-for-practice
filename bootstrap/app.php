3e509c1a1e1aec2d029d8e764cf3bc18e41c5bf9
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-03-06 20:29:33 +0100

    Creating helper file and function base_path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ session_start();
 require_once __DIR__.'/../vendor/autoload.php';
 
 try {
-    $dotenv = \Dotenv\Dotenv::create(__DIR__.'/..');
+    $dotenv = \Dotenv\Dotenv::create(base_path());
     $dotenv->load();
 } catch (\Dotenv\Exception\InvalidPathException $e) {
 }

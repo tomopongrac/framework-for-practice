6362ca2853d5f905a113aca0e637ab7a2d37e7e9
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-03-06 20:32:10 +0100

    Creating helper function env

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,23 @@ if (!function_exists('base_path')) {
         return __DIR__.'/..'.($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 }
+
+if (!function_exists('env')) {
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case $value === 'true';
+                return true;
+            case $value == 'false';
+                return false;
+            default:
+                return $value;
+        }
+    }
+}

2aadd2219bc079090874799e0c2cff7e85d3fdaa
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-03-06 20:01:48 +0100

    Instaling package for load environment variables
    That is the package vlucas/phpdotenv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,3 +3,9 @@
 session_start();
 
 require_once __DIR__.'/../vendor/autoload.php';
+
+try {
+    $dotenv = \Dotenv\Dotenv::create(__DIR__.'/..');
+    $dotenv->load();
+} catch (\Dotenv\Exception\InvalidPathException $e) {
+}

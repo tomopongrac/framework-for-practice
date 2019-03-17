ab272d91339ffa1b4b6d2b5b8348e1947714f2b7
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-03-17 17:25:48 +0100

    Clearing database before each test

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -10,6 +10,8 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class FeatureContext extends MinkContext implements Context
 {
+    private static $container;
+
     /**
      * Initializes context.
      *
@@ -19,5 +21,33 @@ class FeatureContext extends MinkContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @BeforeSuite
+     */
+    public static function bootstrapApp()
+    {
+        $environmentEnv = 'behat';
+        require __DIR__.'/../../bootstrap/app.php';
+
+        self::$container = $container;
+
+        $em = $container->get(\Doctrine\ORM\EntityManager::class);
+
+        $tool = new \Doctrine\ORM\Tools\SchemaTool($em);
+        $classes = array(
+            $em->getClassMetadata('App\Models\User'),
+        );
+        $tool->updateSchema($classes);
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function clearData()
+    {
+        $em = self::$container->get(\Doctrine\ORM\EntityManager::class);
+        $em->createQuery('DELETE FROM App\Models\User')->execute();
     }
 }

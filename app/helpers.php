505704611f261dda2678cf0c3cc3da5b220a8376
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-03-20 22:44:44 +0100

    Creating redirect helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!function_exists('redirect')) {
+    function redirect($path)
+    {
+        return new \Zend\Diactoros\Response\RedirectResponse($path);
+    }
+}
+
 if (!function_exists('base_path')) {
     function base_path($path = '')
     {

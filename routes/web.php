8447d78de8b3c61105a7ab94cc2ed6bf07150040
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-06-22 09:30:16 +0200

    Creating GuestMiddleware and applying to routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,16 @@ use Psr\Http\Message\ServerRequestInterface;
 
 $route->group('', function ($route) {
     $route->get('/dashboard', 'App\Controllers\DashboardController::index')->setName('dashboard');
+
+    $route->post('/auth/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
 })->middleware($container->get(\App\Middleware\AuthenticatedMiddleware::class));
 
+$route->group('', function ($route) {
+    $route->get('/auth/signin', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
+    $route->post('/auth/signin', 'App\Controllers\Auth\LoginController::store');
+
+    $route->get('/auth/register', 'App\Controllers\Auth\RegisterController::index')->setName('auth.register');
+    $route->post('/auth/register', 'App\Controllers\Auth\RegisterController::register');
+})->middleware($container->get(\App\Middleware\GuestMiddleware::class));
+
 $route->get('/', 'App\Controllers\HomeController::index')->setName('homepage');
-$route->get('/auth/signin', 'App\Controllers\Auth\LoginController::index')->setName('auth.login');
-$route->post('/auth/signin', 'App\Controllers\Auth\LoginController::store');
-
-$route->post('/auth/logout', 'App\Controllers\Auth\LogoutController::logout')->setName('auth.logout');
-
-$route->get('/auth/register', 'App\Controllers\Auth\RegisterController::index')->setName('auth.register');
-$route->post('/auth/register', 'App\Controllers\Auth\RegisterController::register');
